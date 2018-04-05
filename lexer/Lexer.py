025fb870ae49bdcdbbc94a65c6ca90d72b97b31f
Varisco <fabio.varisco@sap.com>
2018-04-05 16:59:55 -0300

    Included a few more token types and respective regexes

--- lexer/Lexer.py
+++ lexer/Lexer.py
@@ -16,16 +16,26 @@ class Lexer(object):
     def __init__(self, text):
         self.text = text
         self.tokens = []
-        self.keywords = ['void', 'float', 'int', 'if', 'while', 'return']
+        self.keywords = ['void', 'return', '#include',
+                         'float', 'int', 'double', 'string', 'bool'
+                         'if', 'else', 'do', 'while', 'switch', 'case', 'break', 'for']
+
         token_specification = [(WHITESPACE, r'\s+'),
-                               (Token.TYPE_ID, r'[A-Za-z]([A-Za-z0-9]*_?)*'),
+                               (Token.TYPE_STRING_LITERAL, r'".*"'),
+                               (Token.TYPE_ID, r'[A-Za-z][A-Za-z0-9_]*'),
                                (Token.TYPE_ARIT_OP, r'\+|-|\*|/'),
-                               (Token.TYPE_FLOAT, r'\d+\.\d+'),
-                               (Token.TYPE_NUM, r'\d+'),
+                               #(Token.TYPE_FLOAT, r'\d+\.\d+'),
+                               (Token.TYPE_NUM, r'\d+(\.\d+)?'),
                                (Token.TYPE_PAREN_L, r'\('),
                                (Token.TYPE_PAREN_R, r'\)'),
+                               (Token.TYPE_BRACKET_L, r'\{'),
+                               (Token.TYPE_BRACKET_R, r'\}'),
+                               (Token.TYPE_EQUAL, r'='),
                                (Token.TYPE_COMMA, r','),
-                               (Token.TYPE_SEMICOLON, r';')]
+                               (Token.TYPE_SEMICOLON, r';'),
+                               (Token.TYPE_RELATIONAL_OP, r'<|<=|==|!=|>=|>'),
+                               (Token.TYPE_LOGICAL_OP, r'\|\||&&')
+                               ]
         
         self.tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
     
@@ -45,7 +55,7 @@ class Lexer(object):
             print(t)
 
 if __name__ == "__main__":
-    text = ['6 + 1', '1.1 * 1221', 'void main1_221()']
+    text = ['6 + 1', '1.1 * 1221', 'void main1_221()', 'idfadsf = "literal - ?  / <> _ sdf";']
     lexer = Lexer(text)
     lexer.evaluate()
     lexer.printTokens()
